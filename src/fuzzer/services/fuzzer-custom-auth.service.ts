82869742a49082a7ab2064e21e9bd3f400f93394
Huy Pham <huypham@pridetek.com>
2017-02-19 20:29:06 -0500

    tests POST with session id

--- src/fuzzer/services/fuzzer-custom-auth.service.ts
+++ src/fuzzer/services/fuzzer-custom-auth.service.ts
@@ -27,24 +27,65 @@ function dvwaAuth(config: FuzzerConfig) {
   let postURL = url.resolve(config.url, postTargetPath);
   console.log(postURL);
 
-  requestPOST({
-    url: postURL,
-    form: { 'username': 'admin', 'password': 'password', 'Login': 'Login' }
-  }).subscribe(res => {
-    // console.log(res.res.headers);
-    getCookie(res);
-  })
+  requestGET({ url: config.url })
+    .map(getCookie)
+    .filter(header => header ? true : false)
+    .map(extractCookieHeader)
+    // .flatMap(requestPOST())
+    .subscribe(cookieHeader => {
+      // let cookieHeader = extractCookieHeader(rawSetCookie);
+      console.log(cookieHeader);
+
+      requestPOST({
+        url: postURL,
+        form: { 'username': 'admin', 'password': 'password', 'Login': 'Login' },
+        headers: { 'Cookie': cookieHeader }
+
+      }).subscribe(res => {
+        console.log(chalk.green('Request Headers'));
+        console.log(res.res.request.headers);
+        console.log(chalk.green('Request Response Headers'));
+        console.log(res.res.headers);
+
+      })
+
+    })
+
 
 
 }
 
-function getCookie(res: RequestResponse) {
-  console.log(res);
-  console.log(res.res.headers);
+function getCookie(res: RequestResponse): Array<string> {
+
   // extract cookie
   let setCookie = res.res.headers['Set-Cookie']
   let headerKeys = Object.keys(res.res.headers);
-  let cookieHeaders = headerKeys.filter(header => new RegExp('cookie', 'g').test(header));
-  console.log(cookieHeaders);
+  let cookieHeaderKeys = headerKeys.filter(header => new RegExp('cookie', 'g').test(header));
+  let cookieHeader = res.res.headers[cookieHeaderKeys[0]];
+  return cookieHeader;
+}
+
+function extractCookieHeader(header: Array<String>) {
+  let sid: string;
+  let security: string;
+  header.map(value => {
+    let subStrings = value.split(';');
+    subStrings.map(subString => {
+      if (subString.toLowerCase().indexOf('sessid') >= 0) {
+        sid = subString;
+      }
+      if (subString.toLowerCase().indexOf('security') >= 0) {
+        security = subString
+      }
+
+    })
+  })
+  let cookieHeader = [security, sid].join('; ')
+
+  return cookieHeader;
+}
+
+
+export interface CookieHeader {
 
 }
